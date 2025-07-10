f67472aedd2160666c01301d15bc75201d0c99d5
Divyanshu <divyanshu.abd9911@gmail.com>
2025-07-10 15:06:16 +0530

    first commit

---
+++ Backend/src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello......🥱🥱");
+});
+
+app.use("/api/v1/auth", authRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
